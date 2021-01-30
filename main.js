d50eb65354ce6cf49203527f843e7814022af053
MadJangE00 <jiniixz@naver.com>
2021-01-30 15:06:58 +0900

    data change

--- main.js
+++ main.js
@@ -6,13 +6,19 @@ var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
     var pathname = url.parse(_url, true).pathname;
-
     if(pathname === '/'){
       if (queryData.id === undefined){
-        fs.readFile(`data/${queryData.id}`, 'utf8', function(err,description){
+        fs.readdir('./data', function(error, filelist){
           var title = 'Welcome';
-          var description =  'Hello.Node.js';
-          var template = `
+          var description =  'Hello.WEME';
+          var list = '<ul>';
+          var i = 0;
+          while(i < filelist.length){
+            list = list +`<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+            i = i + 1;
+          }
+          list = list +'</ul>';
+         var template = `
           <!doctype html>
           <html>
           <head>
@@ -21,11 +27,7 @@ var app = http.createServer(function(request,response){
           </head>
           <body>
             <h1><a href="/">WEB</a></h1>
-            <ul>
-              <li><a href="/?id=HTML">HTML</a></li>
-              <li><a href="/?id=CSS">CSS</a></li>
-              <li><a href="/?id=JavaScript">JavaScript</a></li>
-            </ul>
+            ${list}
             <h2>${title}</h2>
             <p>${description}</p>
           </body>
@@ -34,6 +36,8 @@ var app = http.createServer(function(request,response){
           response.writeHead(200);
           response.end(template);
         });
+
+
       } else {
         fs.readFile(`data/${queryData.id}`, 'utf8', function(err,description){
           var title = queryData.id;
